70d5b1723753e1f04ecbe00740e919f777a4bb88
POOJA <POOJA@DESKTOP-EQGI114>
2018-09-23 09:24:26 +0530

    select is included

--- proj/src/main/java/EMPLOYEEdb/TESTselect.java
+++ proj/src/main/java/EMPLOYEEdb/TESTselect.java
@@ -1,19 +1,24 @@
 package EMPLOYEEdb;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.sql.ResultSet;
 
-
-public class TESTinsert {
+public class TESTselect {
 
 	public static void main(String[] args) {
 			
 		try {
 			Connection conn=DriverManager.getConnection("jdbc:h2:tcp://localhost/~/EmployeeDB","pooja","pooja");
 			Statement st=conn.createStatement();
-			String str="insert into employee values(3,'pears');";
-			st.execute(str);
+			String str="select * from employee;";
+			ResultSet rs=st.executeQuery(str);
+			while(rs.next())
+			{
+				int id=rs.getInt(1);
+				String name=rs.getString(2);
+				System.out.println(id  +" "+" "+  name);
+			}
 			System.out.println("Success");
 	}
 		catch(Exception ex)
@@ -23,3 +28,4 @@ public class TESTinsert {
 
 	}
 }
+
